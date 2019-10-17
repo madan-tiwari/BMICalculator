f62fc82e0ac96ff4df73f8dc92bcdba7aca83c8d
madan-tiwari <rebelmadan13@gmail.com>
2019-10-17 21:15:22 +0545

    Initial commit

---
+++ app/src/main/java/com/madantiwari/bmicalculator/MainActivity.java
@@ -0,0 +1,50 @@
+package com.madantiwari.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etWeight, etHeight;
+    TextView result;
+    Button btncalc;
+    Float Height, Weight, BMI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etWeight = findViewById(R.id.etWgt);
+        etHeight = findViewById(R.id.etHgt);
+        result = findViewById(R.id.tvResult);
+        btncalc = findViewById(R.id.btnCalc);
+        btncalc.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (TextUtils.isEmpty(etWeight.getText().toString())) {
+            etWeight.setError("Please Enter Weight");
+            return;
+        } else if (TextUtils.isEmpty(etHeight.getText().toString())) {
+            etHeight.setError("Please Enter Height");
+            return;
+        }
+
+        Height=Float.parseFloat(etHeight.getText().toString());
+        Weight=Float.parseFloat(etWeight.getText().toString());
+        BMI= Weight/(Height*Height);
+
+        result.setText(""+BMI);
+    }
+
+}
